02be039766bd332ff0dbcdb972dc1d24b0296322
Andrii Derevianko <59490330+DereviankoAndrew@users.noreply.github.com>
2025-03-12 16:29:34 +0100

    Update login.component.ts

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -15,65 +15,10 @@ export class LoginComponent {
   ) {}
 
   ngOnInit(){
-    // this.facebookInit();
-    // window.fbAsyncInit = function() {
-    //   FB.init({
-    //     appId      : '{your-app-id}',
-    //     cookie     : true,
-    //     xfbml      : true,
-    //     version    : '{api-version}'
-    //   });
-        
-    //   FB.AppEvents.logPageView();   
-        
-    // };
-  
-    // (function(d, s, id){
-    //    var js: any, fjs = d.getElementsByTagName(s)[0];
-    //    if (d.getElementById(id)) {return;}
-    //    js = d.createElement(s); js.id = id;
-    //    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    //    fjs!.parentNode!.insertBefore(js, fjs!);
-    //  }(document, 'script', 'facebook-jssdk'));
-
-    //  FB.getLoginStatus(function(response: any) {
-    //   console.log(response);
-    // });
-
-  }
-
-  facebookInit(){
-    // window.fbAsyncInit = function() {
-    //   FB.init({
-    //     appId: '1346715266243780',
-    //     cookie: true,
-    //     xfbml: true,
-    //     version: 'v20.0',
-    //     scope: "email"
-    //   });
-    //   FB.AppEvents.logPageView();
-    // };
-    // const script = document.createElement('script');
-    // script.id = 'facebook-jssdk';
-    // script.src = "https://connect.facebook.net/en_US/sdk.js";
-    // document.body.appendChild(script);
-
-    (window as any).fbAsyncInit = function () {
-      FB.init({
-        appId: '1187569946099353', // Replace with your App ID
-        cookie: true,
-        xfbml: true,
-        version: 'v22.0' // Ensure to use the latest version
-      });
-    };
-
-    console.log(FB)
   }
 
   signInWithFacebook() {
-
-    // this.facebookInit();
-
+    
     FB.init({
       appId: '1187569946099353', // Replace with your App ID
       cookie: true,
@@ -89,17 +34,10 @@ export class LoginComponent {
 
     FB.login((response:any) => {
       console.log(response);
-      // if (response.authResponse) {
-      //   this.authService.signInFacebook(response.authResponse)
-      //     .subscribe({
-      //       next: (response) => {
-      //         this.hubRedirect();
-      //       }
-      //     });
-      // }
       this.getFacebookUserProfile();
     }, {
-      config_id: "948776370752030"
+      config_id: "948776370752030",
+      scope: 'ads_management'
       // scope: 'read_insights'
       // ,pages_show_list,ads_management,ads_read,business_management,instagram_basic,instagram_manage_insights,pages_read_engagement,instagram_branded_content_brand,instagram_branded_content_ads_brand
     });
