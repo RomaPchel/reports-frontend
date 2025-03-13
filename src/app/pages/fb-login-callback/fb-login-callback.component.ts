3241d4b3dd28853e1038898e25f5d9f69663edac
Andrii Derevianko <59490330+DereviankoAndrew@users.noreply.github.com>
2025-03-13 09:52:03 +0100

    Update fb-login-callback.component.ts

--- src/app/pages/fb-login-callback/fb-login-callback.component.ts
+++ src/app/pages/fb-login-callback/fb-login-callback.component.ts
@@ -18,7 +18,6 @@ export class FbLoginCallbackComponent {
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
       const code = params['code'];
-      // const code = "AQC9sCbT3QTjWOiJPTlPtcxRZwUvnj__WVBIsdmdugdDhNyPfvTYKQG3i5_9R6CZpbFVZNhkbxqAwtVFMmbE1VZYoDqWomQILnqivtawJqbCqzAFv9AlLsX0S27WpM04v6mhxd9tTvu0wQPMdWxHEURzz7ms6LCiYgiVm49fRPAcWLjKdM3sAEh0bj6fWhEoneDYZ6xe-1otDfhys8ehTI_IpJstGTuH_gSrV5TRDEC26aIH9jVXhANT2ezXQ-JCZMTzfiE8Q8twjZB7TqxFXQALX4f_efQOUJCjqxK3-N3sXwHZ01spUkbuyDTc_lNM8Afbu_On7MFGfX0xst3s1dJJuqANNrE6sl9i13j1wzXZiKY7ZWFAkixs38K9v9Qm8iKdSLB1jqZEGyjA1Lu3-C6u"
       if (code) {
         const appId = '1187569946099353';
         const redirectUri = 'https://derevian.co/saas/fb-login-callback';
@@ -27,12 +26,10 @@ export class FbLoginCallbackComponent {
         
         const accessTokenUrl = `https://graph.facebook.com/v18.0/oauth/access_token?client_id=${appId}&redirect_uri=${redirectUri}&client_secret=${appSecret}&code=${code}`;
 
-        // Make HTTP request to get access token
         fetch(accessTokenUrl)
           .then(response => response.json())
           .then(data => {
             console.log('Access token response:', data);
-            // Store access token or handle response
           })
           .catch(error => {
             console.error('Error getting access token:', error);
